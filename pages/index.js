7265cff97b72f5bea51f487173f7b76d72a58f8d
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-10-21 17:10:17 +0200

    na indeksie wjezdzaja 2 zdjecia razem z animacja, jedno z prawej 2 z lewej :)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 import Layout from "@/components/Layout";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/motion";
 
 export default function Home() {
   return (
     <Layout>
-      <div className="w-full h-full flex justify-center items-center"></div>
+      <div className="w-full h-full flex justify-center items-center">
+        <motion.div
+          variants={fadeIn("right", "spring", 0.5, 1)}
+          initial="hidden"
+          whileInView="show"
+          className="fixed bottom-[50px] left-[200px]"
+        >
+          <img src="nike.png" className="w-[400px]" alt="" />
+        </motion.div>
+        <motion.div
+          variants={fadeIn("left", "spring", 0.7, 1)}
+          initial="hidden"
+          whileInView="show"
+          className="fixed top-[50px] right-[200px]"
+        >
+          <img src="nike2.png" className="w-[400px]" alt="" />
+        </motion.div>
+      </div>
     </Layout>
   );
 }
