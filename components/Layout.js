6a84fc67bddb0249b1fbc47843207d196dd84b57
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-10-21 16:40:56 +0200

    Layout ready :)

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,12 @@ import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-[100vh]">
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="min-h-screen pt-[40px] flex justify-center">
-        <div className="bg-color-500 absolute top-[150px] left-[120px] w-[200px] sm:w-[300px] h-[200px] rounded-full blur-[80px]" />
-        <div className="bg-color-500 absolute bottom-[120px] right-[100px] w-[200px] sm:w-[350px] h-[200px] rounded-full blur-[80px]" />
-        <div className="bg-primary absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 size-[300px] sm:size-[500px] blur-[150px] rounded-full " />
+      <div className=" flex justify-center">
+        <div className="bg-color-600 absolute top-[150px] left-[120px] w-[200px] sm:w-[300px] h-[200px] rounded-full blur-[80px]" />
+        <div className="bg-color-600 absolute bottom-[120px] right-[100px] w-[200px] sm:w-[350px] h-[200px] rounded-full blur-[80px]" />
+        <div className="bg-primary absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 size-[300px] sm:size-[500px] blur-[150px] rounded-full" />
         <div className="w-full p-5">{children}</div>
       </div>
     </div>
